9e5e969679bc42cf41105d8a951de354e1c1c4d1
drsimplegraffiti <abayomiogunnusi@gmail.com>
2022-11-09 05:48:57 +0100

    Chores: Added dark theme

--- main.js
+++ main.js
@@ -6,10 +6,14 @@ window.onscroll = function () {
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById('scrolled').style.backgroundColor = '#fff';
-    // document.getElementById('scrolled').style.boxShadow = '0 0 1px rgba(0,0,0,0.1)';
-
+    document.getElementById('scrolled').style.boxShadow =
+      '0 0 10px rgba(0,0,0,0.1)';
+    // handle font awesome icon color
+    document.getElementsByClassName('fa-regular').style.color = '#000';
   } else {
     document.getElementById('scrolled').style.backgroundColor = 'transparent';
     document.getElementById('scrolled').style.boxShadow = 'none';
+    // handle font awesome icon color
+    document.getElementsByClassName('fa-regular').style.color = 'green';
   }
 }
